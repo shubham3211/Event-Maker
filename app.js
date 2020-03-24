31e798c77c681aebb7b029860964668d5516d376
shubham <kumarsinghshubham32111@gmail.com>
2020-03-24 17:06:08 +0530

    backend api done

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+let bodyParser = require('body-parser');
+let port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
+
+let events = {};
+
+app.get('/events', (req, res) => {
+  res.send({
+    events
+  });
+})
+
+app.put('/events/:id', (req, res) => {
+  if(!events[req.params.id]){
+    res.send({
+      status: false,
+      message: 'Event doesnot exist'
+    })
+  }else{
+    if(req.body.name){
+      events[req.params.id].name = req.body.name;
+    }
+    if(req.body.imageUrl){
+      events[req.params.id].imageUrl = req.body.imageUrl;
+    }
+    if(req.body.location){
+      events[req.params.id].location = req.body.location;
+    }
+    res.send({
+      status: true,
+      message: "event updated"
+    })
+  }
+})
+
+app.post('/add-event', (req, res) => {
+  let id = Object.keys(events).length+1;
+  events[id] = {
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+    location: req.body.location
+  }
+  res.send({
+    status: true,
+    message: "event added"
+  })
+})
+
+app.put('/delete-event/:id', (req, res) => {
+  if(events[req.params.id]){
+    delete events[req.params.id];
+    res.send({
+      status: true,
+      message: "Event deleted"
+    })
+  }else{
+    res.send({
+      status: false,
+      message: "Please send right parameter"
+    })
+  }
+})
+
+app.listen(5000, () => console.log('listening on port 5000'));  
