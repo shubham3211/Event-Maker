519e833ddea23bfe377f0dec21904d09bb827f82
shubham <kumarsinghshubham32111@gmail.com>
2020-03-24 17:25:25 +0530

    log created

--- app.js
+++ app.js
@@ -2,16 +2,11 @@ const express = require('express');
 const app = express();
 let bodyParser = require('body-parser');
 let port = process.env.PORT || 5000;
+var cors = require('cors')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+app.use(cors())
 
 let events = {};
 
